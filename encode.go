24a7a5c5e8a495b2dc7f9ee7dcda9217465f9100
lemon-mint <ice.lemons.mint@gmail.com>
2022-03-09 13:08:31 +0900

    ImageEncode, ImageDecode

---
+++ encode.go
@@ -0,0 +1,96 @@
+package bin2jpg
+
+import (
+	"bytes"
+	"encoding/binary"
+	"image"
+	"image/color"
+	"io"
+
+	"github.com/andybalholm/brotli"
+)
+
+func ImageEncode(data []byte) image.Image {
+	const width = 5 * 5 * 8
+
+	var compressed bytes.Buffer
+	compressed.Write([]byte{0, 0, 0, 0})
+	w := brotli.NewWriterLevel(&compressed, brotli.BestCompression)
+	w.Write(data)
+	w.Close()
+	data = compressed.Bytes()
+	binary.LittleEndian.PutUint32(data[:4], uint32(len(data)))
+
+	img := image.NewGray(image.Rect(0, 0, width, (len(data)*8/width+1)*5+3))
+	var x, y int
+	bound := img.Bounds()
+	bound_x := bound.Max.X
+
+	for i, b := range data {
+		bit := byte(b)
+		for j := 0; j < 8; j++ {
+			for k := 0; k < 5; k++ {
+				if x%bound_x == 0 && i != 0 {
+					y += 1
+					x = 0
+				}
+				if (bit & (1 << uint(j))) != 0 {
+					img.SetGray(x, y, color.Gray{255})
+				} else {
+					img.SetGray(x, y, color.Gray{0})
+				}
+				x++
+			}
+		}
+	}
+
+	return img
+}
+
+func isBlack(c color.Color) bool {
+	r, g, b, _ := c.RGBA()
+	return r < 128 || g < 128 || b < 128
+}
+
+func ImageDecode(img image.Image) ([]byte, error) {
+	var data []byte
+	bound := img.Bounds()
+	bound_x := bound.Max.X
+	var x, y int
+	y = -1
+
+	readByte := func() byte {
+		var b byte
+
+		for i := 0; i < 8; i++ {
+			var black, white uint8
+			for j := 0; j < 5; j++ {
+				if x%bound_x == 0 {
+					y += 1
+					x = 0
+				}
+				//println("x:", x, "y:", y)
+				if isBlack(img.At(x, y)) {
+					black++
+				} else {
+					white++
+				}
+				x++
+			}
+			if black < white {
+				b |= 1 << uint(i)
+			}
+		}
+		return b
+	}
+
+	var length [4]byte = [4]byte{readByte(), readByte(), readByte(), readByte()}
+	l := binary.LittleEndian.Uint32(length[:])
+
+	for i := 0; i < int(l); i++ {
+		data = append(data, readByte())
+	}
+
+	r := brotli.NewReader(bytes.NewReader(data))
+	return io.ReadAll(r)
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/lemon-mint/bin2jpg
 
 go 1.17
+
+require github.com/andybalholm/brotli v1.0.4
